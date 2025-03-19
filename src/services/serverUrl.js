bce0a8770e734abc8a7580933ef257bb55928306
sebin005 <sebinvalloor26@gmail.com>
2025-03-19 12:06:35 +0530

    1 more bug fixed

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,3 +1,3 @@
-// export const serverUrl = 'https://mediaplayerbe-lee9.onrender.com'
+export const serverUrl = 'https://mediaplayerbe-lee9.onrender.com'
 
-export const serverUrl = 'http://localhost:5000'
+// export const serverUrl = 'http://localhost:5000'
